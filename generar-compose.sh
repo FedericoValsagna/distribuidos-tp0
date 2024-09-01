f324a1539671c81e6f8a4ab08b9587e159946d49
FedericoValsagna <fvalsagna@fi.uba.ar>
2024-09-01 00:47:06 -0300

    Resolucion

--- generar-compose.sh
+++ generar-compose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Nombre del archivo de salida: $1"
 echo "Cantidad de clientes: $2"
-cat templates/compose/head.txt > $1
+cat templates/compose/head.txt | envsubst > $1
 for i in $(seq $2); do
     CLI_ID=$i
     export CLI_ID
