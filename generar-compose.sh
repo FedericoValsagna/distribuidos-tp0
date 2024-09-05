73692a80a574fc0aabd1b841245bea4ab2caee32
FedericoValsagna <fvalsagna@fi.uba.ar>
2024-09-05 11:38:23 -0300

    Fix

--- generar-compose.sh
+++ generar-compose.sh
@@ -4,7 +4,9 @@ echo "Cantidad de clientes: $2"
 cat templates/compose/head.txt | envsubst > $1
 for i in $(seq $2); do
     CLI_ID=$i
+    PATH_COMPLETO= pwd
     export CLI_ID
+    export PATH_COMPLETO
     cat templates/compose/client.txt | envsubst >> $1
 done
 cat templates/compose/tail.txt >> $1
