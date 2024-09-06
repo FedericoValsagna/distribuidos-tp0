5b7cdc096b48dad9a08b50b057439299f1a584b0
FedericoValsagna <fvalsagna@fi.uba.ar>
2024-09-05 23:45:07 -0300

    Added netcast timeout

--- netcat_test.sh
+++ netcat_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update > /dev/null
 apt install netcat-traditional > /dev/null
-echo "Hello World!" | nc server 12345 > /dev/null
+echo "Hello World!" | nc server 12345 -w 2 > /dev/null
 if [ $? -eq 0 ]
 then
     echo "action: test_echo_server | result: success"
