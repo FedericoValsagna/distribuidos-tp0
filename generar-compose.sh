5f0801ef041af46c97ff3bd6ad7c5bdb11d7e4f6
Federico Valsagna <fvalsagna@fi.uba.ar>
2026-03-18 05:04:01 -0300

    Solucion

--- generar-compose.sh
+++ generar-compose.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+TEMP="temp.txt"
 echo "Nombre del archivo de salida: $1"
 echo "Cantidad de clientes: $2"
-
-cat ./template/head.txt > $1
+PWD=$(pwd | tr "/" "$")
+cat ./template/head.txt > $TEMP
 for (( CLIENT=1 ; CLIENT<=$2 ; CLIENT++ )); do
-    cat ./template/client.txt | tr "#" $CLIENT >> $1
+    cat ./template/client.txt | tr "#" $CLIENT >> $TEMP
 done
-cat ./template/tail.txt >> $1
+cat ./template/tail.txt >> $TEMP
+sed -i "s/PWD/$PWD/g" $TEMP
+cat $TEMP | tr "$" "/" > $1
+rm $TEMP
