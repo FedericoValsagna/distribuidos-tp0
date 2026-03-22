7c7583e435ae35464f8674564f57c81c186ac800
Federico Valsagna <fvalsagna@fi.uba.ar>
2026-03-21 23:46:12 -0300

    3rd test fix

--- netcat_test.sh
+++ netcat_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 apt update > /dev/null
 apt install netcat-traditional > /dev/null
-echo "Hello World!" | nc server 12345 -w 2 > /dev/null
-if [ $? -eq 0 ]
+RESPONSE=$(echo "Hello World!" | nc server 12345 -w 2)
+echo $RESPONSE
+if [ "$RESPONSE" = "Hello World!" ];
 then
     echo "action: test_echo_server | result: success"
 else
