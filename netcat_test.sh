d0d077db50073047dcc7db0464718bf44a486737
Federico Valsagna <fvalsagna@fi.uba.ar>
2026-03-21 23:47:34 -0300

    Debug echoes removed

--- netcat_test.sh
+++ netcat_test.sh
@@ -2,7 +2,6 @@
 apt update > /dev/null
 apt install netcat-traditional > /dev/null
 RESPONSE=$(echo "Hello World!" | nc server 12345 -w 2)
-echo $RESPONSE
 if [ "$RESPONSE" = "Hello World!" ];
 then
     echo "action: test_echo_server | result: success"
