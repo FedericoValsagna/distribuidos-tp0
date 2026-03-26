41a58421e49d827d909f7265e0da7ee5dbfd3407
Federico Valsagna <fvalsagna@fi.uba.ar>
2026-03-26 14:58:23 -0300

    Fixed broken protocol

--- server/common/server.py
+++ server/common/server.py
@@ -47,7 +47,7 @@ class Server:
             bets = parse_message(msg)
             store_bets(bets)
             logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(bets)}')
-            msg = "Apuesta recibida"
+            msg = "R"
             msg = fill_padding(msg)
             client_sock.send(msg.encode('utf-8'))
         except OSError as e:
